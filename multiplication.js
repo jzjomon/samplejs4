47ca36517ff588403190e46293a57f8f28308b41
jomon jose <jzjomon@gmail.com>
2023-05-21 12:28:09 +0530

    screenshot added

--- multiplication.js
+++ multiplication.js
@@ -1,5 +1,5 @@
 var readlineSync = require("readline-sync");
 let num = readlineSync.question("enter a number");
 for(let i=1;i<=10;i++){
-    console.log(i,"*",num,"=",num*i);
+    console.log(i,"x",num,"=",num*i);
 }
