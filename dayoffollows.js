c9387d69662d8d4468e1cb2c1bd8978281138bd7
jomon jose <jzjomon@gmail.com>
2023-05-21 12:18:41 +0530

    screenshot folder added

--- dayoffollows.js
+++ dayoffollows.js
@@ -1,2 +1,28 @@
 var readlineSync = require("readline-sync");
-let num = readlineSync.question("enter a number");
+let day = readlineSync.question("enter a number");
+num = parseInt(day);
+switch(num){
+    case 1 :
+        console.log("sunday");
+        break;
+    case 2 :
+        console.log("monday");
+        break;
+    case 3 :
+        console.log("tuesday");
+        break;
+    case 4 :
+        console.log("wednesday");
+        break;
+    case 5 :
+        console.log("thursday");
+        break;
+    case 6 :
+        console.log("friday");
+        break;
+    case 7 :
+        console.log("saturday");
+        break;
+    default :
+    console.log("enter a valid number");
+}
