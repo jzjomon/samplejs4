b54e5c96f56797b8571b70e5e4f2da04598fb95a
jomon jose <jzjomon@gmail.com>
2023-05-21 15:58:52 +0530

    number of even number in the array is finished

---
+++ arrayeven.js
@@ -0,0 +1,12 @@
+var readlineSync = require("readline-sync");
+let num = readlineSync.question("the size of the array");
+console.log("enter the values of the array");
+const a=[];
+let b=0;
+for(let i=0;i<num;i++){
+    a[i]=readlineSync.question("");
+    if(i%2==0){
+        b=b+1;
+    }
+}
+console.log("number of even numbers in the given array is",b);
